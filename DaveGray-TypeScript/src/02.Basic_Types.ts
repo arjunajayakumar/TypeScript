7f6352202362f1b09e62c71a7e14867e8bd0a853
arjun <arjune64@gmail.com>
2023-04-24 15:28:07 +0530

    covered basic types

---
+++ DaveGray-TypeScript/src/02.Basic_Types.ts
@@ -0,0 +1,25 @@
+// string
+let myName: string = "Arjun";
+// number
+let meaningOfLife: number;
+// boolean
+let isLoading = true;
+// any
+let album: any;
+// union
+let userId: string | number;
+
+myName = "John";
+meaningOfLife = 42;
+isLoading = true;
+album = 32;
+album = "any";
+album = false;
+userId = "12";
+userId = 12;
+
+const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+console.log();
