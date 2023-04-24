023c37c2b0732be27480990b94647a9ad33416e6
arjun <arjune64@gmail.com>
2023-04-24 15:27:28 +0530

    basic typescript setup

---
+++ DaveGray-TypeScript/build/js/02.Basic_Types.js
@@ -0,0 +1,22 @@
+"use strict";
+// string
+let myName = "Arjun";
+// number
+let meaningOfLife;
+// boolean
+let isLoading = true;
+// any
+let album;
+// union
+let userId;
+myName = "John";
+meaningOfLife = 42;
+isLoading = true;
+album = 32;
+album = "any";
+album = false;
+userId = "12";
+userId = 12;
+const sum = (a, b) => {
+    return a + b;
+};
---
+++ DaveGray-TypeScript/src/main.ts
@@ -0,0 +1,9 @@
+let username = "Arju";
+console.log(username);
+
+let a: number = 12;
+let b: number = 6;
+let c = 2;
+
+console.log(a / b);
+console.log(c * b);
